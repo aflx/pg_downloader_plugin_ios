47a157c3dfe7d42fd6bfd3d84ba8d67af8ad8cf9
Alexander Keller <ak@aflx.de>
2011-08-22 08:48:07 +0200

    added comment

--- Downloader.m
+++ Downloader.m
@@ -5,6 +5,9 @@
 //  http://www.aflx.de
 //  ak@aflx.de
 //
+//  I have adapted the classname and signature of the method of this plugin for
+//  Android and iOS.
+//
 //  Copyright 2011 Alexander Keller All rights reserved.
 //
 //  Thanks to Aaron K. Saunders
